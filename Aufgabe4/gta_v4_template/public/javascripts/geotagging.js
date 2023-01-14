6b2a42935a081da4492ab20f64e3e11c98e98ee8
Stefan Maier <mast1027@h-ka.de>
2023-01-14 16:29:45 +0000

    finishing

--- Aufgabe4/gta_v4_template/public/javascripts/geotagging.js
+++ Aufgabe4/gta_v4_template/public/javascripts/geotagging.js
@@ -9,6 +9,22 @@
 // Try to find this output in the browser...
 console.log("The geoTagging script is going to start...");
 
+let memoryTaglist = JSON.parse(document.getElementById("mapView").getAttribute("data-tags"));
+const NUMBER_OF_TAGS = 5;
+
+function initPagination() {
+    let maxPages = Math.ceil(memoryTaglist.length / NUMBER_OF_TAGS);
+
+    document.getElementById("paginationNext").disabled = false;
+    document.getElementById("paginationPrev").disabled = true;
+
+    if (memoryTaglist.length < 1) {document.getElementById("currentPage").innerHTML = "0";}
+    else {document.getElementById("currentPage").innerHTML = "1";}
+    document.getElementById("listElements").innerHTML = memoryTaglist.length;
+
+    document.getElementById("maxPage").innerHTML = maxPages.toString();
+    getPaginationTags(1).then(updatePagination);
+}
 
 function updateMap_by_LatLon(lat, lon) {
     taglist_json = JSON.parse(decodeURIComponent(document.getElementById("mapView").getAttribute("data-tags")));
@@ -26,6 +42,7 @@ function updateMap_by_tags(taglist) {
     mapURL = mm.getMapUrl(lat, lon, taglist);
 
     document.getElementById("mapView") . src = mapURL;
+    return taglist;
 }
 
 function updateLocation() {
@@ -45,14 +62,61 @@ function updateLocation() {
                 .setAttribute("value", locationHelper.longitude);
             updateMap_by_LatLon(locationHelper.latitude, locationHelper.longitude);
         });
-        //TODO paginationSetup();
+        initPagination();
     }
 }
 
-function updateTagList(geotags) {
-    actualTaglist = JSON.parse(geotags);
-    paginationSetup();
+function updateTagList(tags) {
+    memoryTaglist = tags;
+    initPagination();
     return parseInt(document.getElementById("currentPage").innerHTML);
+}
+
+function updatePagination(tags) {
+    let currentPage = parseInt(document.getElementById("currentPage").innerHTML);
+    let taglist = JSON.parse(tags);
+    if (taglist !== undefined) {
+        let list = document.getElementById("discoveryResults");
+        list.innerHTML = "";
+
+        taglist.forEach(function (tag) {
+            let li = document.createElement("li");
+            li.innerHTML = tag.name + " (" + tag.latitude + "," + tag.longitude + ") " + tag.tag;
+            list.appendChild(li);
+        });
+    }
+
+    let maxPageNumber = Math.ceil(memoryTaglist.length / NUMBER_OF_TAGS);
+
+    if (currentPage === maxPageNumber && maxPageNumber <= 1) {
+        document.getElementById("paginationNext").disabled = true;
+        document.getElementById("paginationPrev").disabled = true;
+    } else if (currentPage < maxPageNumber && currentPage > 1) {
+        document.getElementById("paginationNext").disabled = false;
+        document.getElementById("paginationPrev").disabled = false;
+    } else if (currentPage < maxPageNumber) {
+        document.getElementById("paginationNext").disabled = false;
+        document.getElementById("paginationPrev").disabled = true;
+    } else if (currentPage === maxPageNumber) {
+        document.getElementById("paginationNext").disabled = true;
+        document.getElementById("paginationPrev").disabled = false;
+    }
+
+    document.getElementById("currentPage").innerHTML = currentPage.toString();
+    document.getElementById("listElements").innerHTML = memoryTaglist.length;
+    document.getElementById("maxPage").innerHTML = maxPageNumber.toString();
+}
+
+/**
+ * fetch for Pagination-Tags (5)
+ */
+ async function getPaginationTags(currentPage) {
+    let geotags = await fetch("http://localhost:3000/api/geotags/page/" + currentPage, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(memoryTaglist),
+    });
+    return await geotags.json();
 }
 
 /**
@@ -64,7 +128,8 @@ function updateTagList(geotags) {
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(tag),
     });
-    return await response.json();
+    // cast to array
+    return [await response.json()];
 }
 
 /**
@@ -92,9 +157,8 @@ function updateTagList(geotags) {
         hashtag: document.getElementById("hashtag").value
     }
 
-    post(geotag).then(getUpdateMap);
-    // TODO post(geotag).then(getUpdateMap).then(updateList).then(getPaginationTags).then(updatePagination);
-    
+    post(geotag).then(updateMap_by_tags).then(updateTagList).then(getPaginationTags).then(updatePagination);
+
     // empty fields
     document.getElementById("name").value = "";
     document.getElementById("hashtag").value = "";
@@ -109,13 +173,28 @@ document.getElementById("discoveryFilterForm").addEventListener("submit", functi
 
     let searchTerm = document.getElementById("searchterm").value;
 
-    // TODO getTagList(searchTerm).then(getUpdateMap).then(updateList).then(getPaginationTags).then(updatePagination)
-    getTags(searchTerm).then(updateMap_by_tags).catch(error => alert("Search term does not exist"));
+    getTags(searchTerm).then(updateMap_by_tags).then(updateTagList).then(getPaginationTags).then(updatePagination).catch(error => alert("Search term does not exist"));
 });
 
 
 
+document.getElementById("paginationNext").addEventListener("click", function (evt) {
+    evt.preventDefault();
 
+    let currentPage = parseInt(document.getElementById("currentPage").innerHTML) + 1;
+    document.getElementById("currentPage").innerHTML = currentPage.toString();
+
+    getPaginationTags(currentPage).then(updatePagination);
+});
+
+document.getElementById("paginationPrev").addEventListener("click", function (evt) {
+    evt.preventDefault();
+
+    let currentPage = parseInt(document.getElementById("currentPage").innerHTML) - 1;
+    document.getElementById("currentPage").innerHTML = currentPage.toString();
+
+    getPaginationTags(currentPage).then(updatePagination);
+});
 
 
 
